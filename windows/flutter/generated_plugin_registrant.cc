376a363ebe18b85f93784e58b026f8a9cd7edcc3
yvan vander sanden <yvan@attr-x.net>
2026-05-03 01:04:09 +0200

    feat: remove py_engine_desktop dependency and related references

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -7,14 +7,11 @@
 #include "generated_plugin_registrant.h"
 
 #include <audioplayers_windows/audioplayers_windows_plugin.h>
-#include <py_engine_desktop/py_engine_desktop_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("AudioplayersWindowsPlugin"));
-  PyEngineDesktopPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("PyEngineDesktopPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -4,7 +4,6 @@
 
 list(APPEND FLUTTER_PLUGIN_LIST
   audioplayers_windows
-  py_engine_desktop
   url_launcher_windows
 )
 
